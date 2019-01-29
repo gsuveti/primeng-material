4ba54241529d8101dc7130125e6551e66bb64e3e
gsuveti <george.suveti@gmail.com>
2019-01-29 19:02:27 +0200

    fix treetable

--- libs/primeng-components/src/lib/tree-table/tree-table-scrollable/tree-table-scrollable.component.ts
+++ libs/primeng-components/src/lib/tree-table/tree-table-scrollable/tree-table-scrollable.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { NodeService } from '../../node.service';
-import { TreeNode } from 'primeng/primeng';
+import {Component, OnInit} from '@angular/core';
+import {NodeService} from '../../node.service';
+import {TreeNode} from 'primeng/primeng';
 
 @Component({
   selector: 'primeng-material-tree-table-scrollable',
@@ -8,22 +8,86 @@ import { TreeNode } from 'primeng/primeng';
   styleUrls: ['./tree-table-scrollable.component.scss']
 })
 export class TreeTableScrollableComponent implements OnInit {
-  lazyFiles: TreeNode[];
+  files: TreeNode[];
 
-  constructor(private nodeService: NodeService) {}
+  cols: any[];
 
-  ngOnInit() {
-    this.nodeService
-      .getLazyFilesystem()
-      .then(files => (this.lazyFiles = files));
+  totalRecords: number;
+
+  loading: boolean;
+
+  constructor(private nodeService: NodeService) {
   }
 
-  nodeExpand(event) {
-    if (event.node) {
-      // in a real application, make a call to a remote url to load children of the current node and add the new nodes as children
-      this.nodeService
-        .getLazyFilesystem()
-        .then(nodes => (event.node.children = nodes));
-    }
+  ngOnInit() {
+    this.cols = [
+      {field: 'name', header: 'Name'},
+      {field: 'size', header: 'Size'},
+      {field: 'type', header: 'Type'}
+    ];
+
+    // in a production application, retrieve the logical number of rows from a remote datasource
+    this.totalRecords = 1000;
+
+    this.loading = true;
+  }
+
+  loadNodes(event) {
+    this.loading = true;
+
+    // in a production application, make a remote request to load data using state metadata from event
+    // event.first = First row offset
+    // event.rows = Number of rows per page
+    // event.sortField = Field name to sort with
+    // event.sortOrder = Sort order as number, 1 for asc and -1 for dec
+    // filters: FilterMetadata object having field as key and filter value, filter matchMode as value
+
+    // imitate db connection over a network
+    setTimeout(() => {
+      this.loading = false;
+      this.files = [];
+
+      for (let i = 0; i < event.rows; i++) {
+        let node = {
+          data: {
+            name: 'Item ' + (event.first + i),
+            size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+            type: 'Type ' + (event.first + i)
+          },
+          leaf: false
+        };
+
+        this.files.push(node);
+      }
+    }, 1000);
+  }
+
+  onNodeExpand(event) {
+    this.loading = true;
+
+    setTimeout(() => {
+      this.loading = false;
+      const node = event.node;
+
+      node.children = [
+        {
+          data: {
+            name: node.data.name + ' - 0',
+            size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+            type: 'File'
+          },
+        },
+        {
+          data: {
+            name: node.data.name + ' - 1',
+            size: Math.floor(Math.random() * 1000) + 1 + 'kb',
+            type: 'File'
+          }
+        }
+      ];
+
+      this.files = [...this.files];
+    }, 250);
+
   }
 }
